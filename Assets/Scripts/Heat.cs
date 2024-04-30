825a3ba6cce7a68cf3ed8645689ff07c85e80b43
DESKTOP-FKI4481\zeyad <zeyad.medhat2000@gmail.com>
2024-04-30 23:10:06 +0300

    -Added cold effect on player

--- Assets/Scripts/Heat.cs
+++ Assets/Scripts/Heat.cs
@@ -9,16 +9,25 @@ public class Heat : MonoBehaviour
 {
     //1- If player is inside the fire collision radius then decrease the slider **done**
     //2- if player is outside the fire collision radius then increase the slider **done**
-    //3- if player reaches 100% of the slider then game over
+    //3- player's color nears blue as the meter increases **done**
+    //4- if player reaches 100% of the slider then game over
 
     public GameObject Player;
     public UnityEngine.UI.Slider slider;
 
+
     private float slidervalue;
     private bool iswarming;
+
+
+    private Color lerpedColor = Color.white;
+    SpriteRenderer Renderer;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        Renderer = GetComponent<SpriteRenderer>();
         
     }
 
@@ -32,15 +41,20 @@ public class Heat : MonoBehaviour
     }
     void FixedUpdate()
     {
+        Debug.Log(slider.value);
         if (iswarming && slider.value != 0f)
         {
-            slidervalue = slider.value - 0.001f;
+            slidervalue = slider.value - 0.002f;
             slider.value = slidervalue;
+            lerpedColor = Color.Lerp(Color.white, Color.cyan, slider.value);
+            Renderer.material.color = lerpedColor;
         }
         if (!iswarming && slider.value != 1f)
         {
             slidervalue = slider.value + 0.001f;
             slider.value = slidervalue;
+            lerpedColor = Color.Lerp(Color.white, Color.cyan, slider.value);
+            Renderer.material.color = lerpedColor;
         }
     }
     void OnTriggerEnter2D(Collider2D col)
